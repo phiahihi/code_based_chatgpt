c25c62cbbfca71ec04f23800b1bf44609f1d744b
Quang Huy <89730025+phiahihi@users.noreply.github.com>
2023-02-11 14:12:13 +0700

    add

---
+++ demo_chatgpt/android/app/src/main/kotlin/com/example/demo_chatgpt/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.demo_chatgpt
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ demo_chatgpt/lib/main.dart
@@ -0,0 +1,103 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'ChatGPT Flutter Example',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final _formKey = GlobalKey<FormState>();
+  final _questionController = TextEditingController();
+  String _answer = '';
+
+  Future<void> _getAnswer() async {
+    if (!_formKey.currentState!.validate()) {
+      return;
+    }
+    _formKey.currentState!.save();
+
+    final response = await http.post(
+      Uri.parse('https://api.openai.com/v1/completions'),
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization':
+            'Bearer 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',
+      },
+      body: jsonEncode({
+        'model': "code-davinci-002",
+        'prompt': _questionController.text,
+        "temperature": 0,
+        "max_tokens": 64,
+        "top_p": 1.0,
+        "frequency_penalty": 0.0,
+        "presence_penalty": 0.0,
+        "stop": ["\"\"\""]
+      }),
+    );
+
+    if (response.statusCode == 200) {
+      final data = jsonDecode(response.body);
+      setState(() {
+        _answer = data['choices'][0]['text'];
+      });
+    } else {
+      setState(() {
+        _answer = 'Error';
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('ChatGPT Flutter Example'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextFormField(
+                controller: _questionController,
+                decoration: InputDecoration(labelText: 'Question'),
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return 'Please enter a question';
+                  }
+                  return null;
+                },
+              ),
+              SizedBox(height: 16.0),
+              TextButton(
+                onPressed: _getAnswer,
+                child: Text('Submit'),
+              ),
+              SizedBox(height: 16.0),
+              Expanded(child: Text(_answer)),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
