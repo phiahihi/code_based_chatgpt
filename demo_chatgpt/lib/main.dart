b6a487599356a22dce3567afca72c0cbb6c38421
Quang Huy <89730025+phiahihi@users.noreply.github.com>
2023-02-11 14:22:02 +0700

    add code chatgpt

--- demo_chatgpt/lib/main.dart
+++ demo_chatgpt/lib/main.dart
@@ -101,3 +101,104 @@ class _HomePageState extends State<HomePage> {
     );
   }
 }
+
+// Code của chatgpt
+
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'ChatGPT Flutter Example',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final _formKey = GlobalKey<FormState>();
+  final _questionController = TextEditingController();
+  String _answer = '';
+
+  Future<void> _getAnswer() async {
+    if (!_formKey.currentState.validate()) {
+      return;
+    }
+    _formKey.currentState.save();
+
+    final response = await http.post(
+      'https://api.openai.com/v1/engines/davinci/jobs',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer <API_KEY>',
+      },
+      body: jsonEncode({
+        'prompt': _questionController.text,
+        'max_tokens': 100,
+        'temperature': 0.5,
+      }),
+    );
+
+    if (response.statusCode == 200) {
+      final data = jsonDecode(response.body);
+      setState(() {
+        _answer = data['choices'][0]['text'];
+      });
+    } else {
+      setState(() {
+        _answer = 'Error';
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('ChatGPT Flutter Example'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextFormField(
+                controller: _questionController,
+                decoration: InputDecoration(labelText: 'Question'),
+                validator: (value) {
+                  if (value.isEmpty) {
+                    return 'Please enter a question';
+                  }
+                  return null;
+                },
+              ),
+              SizedBox(height: 16.0),
+              RaisedButton(
+                onPressed: _getAnswer,
+                child: Text('Submit'),
+              ),
+              SizedBox(height: 16.0),
+              Text(_answer),
+            ],
+            
+          ),
+        ),
+      ),
+    );
+  }
+}
